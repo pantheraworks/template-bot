287301f79cd9111653cab931e33c1baf80016230
matias-gonz <maigonzalez@fi.uba.ar>
2024-06-02 22:20:36 +0200

    Add venom app

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
-const nombre: string = "Tomas";
+const venom = require('venom-bot');
 
-console.log(nombre);
+venom
+  .create({
+    session: 'bot'
+  })
+  .then((client) => start(client))
+  .catch((erro) => {
+    console.log(erro);
+  });
+
+const start = (client) => {
+  client.onMessage((message) => {
+    if (message.body === 'Hi' && message.isGroupMsg === false) {
+      client
+        .sendText(message.from, 'Welcome Venom 🕷')
+        .then((result) => {
+          console.log('Result: ', result); //return object success
+        })
+        .catch((erro) => {
+          console.error('Error when sending: ', erro); //return object error
+        });
+    }
+  });
+}
