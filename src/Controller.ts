fb2f6a17fe05f81c2126cb97912e2380665b455f
Tomas Gonzalez <tfgonzalez@fi.uba.ar>
2024-06-03 19:54:43 -0300

    Refactor sendMenu

--- src/Controller.ts
+++ src/Controller.ts
@@ -3,56 +3,63 @@ import User from "./User";
 
 class Controller {
 
-  private client: Whatsapp;
-  private users: Map<string, User>;
+    private client: Whatsapp;
+    private users: Map<string, User>;
 
-  public constructor(client: Whatsapp) {
-    this.client = client;
-    this.users = new Map();
-  }
-
-  public async handleMessage(message: Message) {
-    const user = this.users.get(message.from) || new User(message.from, message.sender.pushname || '');
-    const response = await user.handleMessage(message.body, this);
-    this.users.set(message.from, user);
-    return response;
-  }
-
-  public async sayHiBack(user: User) {
-    const name = user.name;
-    return await this.sendText(user.id, `Hola ${name}!`);
-  }
-
-  public async sendMainOptions(user: User) {
-    const options = [
-      'Mostrar menú',
-      'Realizar pedido',
-      'Consultar pedido'
-    ]
-    const optionText = options.map((option, index) => `${index + 1}. ${option}`).join('\n');
-    const text = `Seleccioná una opción:\n${optionText}`;
-    return await this.sendText(user.id, text);
-  }
-
-  public async sendMenu(user: User): Promise<void> {
-    try {
-      await this.sendText(user.id, 'Este es el menu:');
-      await this.sendImage(user.id, './src/assets/menu_1.jpg', 'menu_1', ' ');
-      await this.sendImage(user.id, './src/assets/menu_2.jpg', 'menu_2', ' ');
-      await this.sendImage(user.id, './src/assets/menu_3.jpg', 'menu_3', ' ');
-      await this.sendImage(user.id, './src/assets/menu_4.jpg', 'menu_4', ' ');
-    } catch (error) {
-      throw error;
+    public constructor(client: Whatsapp) {
+        this.client = client;
+        this.users = new Map();
     }
-  }
 
-  public async sendText(to: string, text: string) {
-    await this.client.sendText(to, text);
-  }
+    public async handleMessage(message: Message) {
+        const user = this.users.get(message.from) || new User(message.from, message.sender.pushname || '');
+        const response = await user.handleMessage(message.body, this);
+        this.users.set(message.from, user);
+        return response;
+    }
 
-  private async sendImage(to: string, path: string, image_name: string, caption: string) {
-    return await this.client.sendImage(to, path, image_name, caption);
-  }
+    public async sayHiBack(user: User) {
+        const name = user.name;
+        return await this.sendText(user.id, `Hola ${name}!`);
+    }
+
+    public async sendMainOptions(user: User) {
+        const options = [
+            'Mostrar menú',
+            'Realizar pedido',
+            'Consultar pedido'
+        ]
+        const optionText = options.map((option, index) => `${index + 1}. ${option}`).join('\n');
+        const text = `Seleccioná una opción:\n${optionText}`;
+        return await this.sendText(user.id, text);
+    }
+
+    public async sendMenu(user: User): Promise<void> {
+        const menuImagesPaths: string[] = [
+            './src/assets/menu_1.jpg',
+            './src/assets/menu_2.jpg',
+            './src/assets/menu_3.jpg',
+            './src/assets/menu_4.jpg'
+        ];
+
+        try {
+            await this.sendText(user.id, 'Este es el menu:');
+            for (const path of menuImagesPaths) {
+                const index = menuImagesPaths.indexOf(path);
+                await this.sendImage(user.id, path, `menu_${index}`, ' ');
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    public async sendText(to: string, text: string) {
+        return await this.client.sendText(to, text);
+    }
+
+    private async sendImage(to: string, path: string, image_name: string, caption: string) {
+        return await this.client.sendImage(to, path, image_name, caption);
+    }
 }
 
 export default Controller;
