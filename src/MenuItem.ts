ddf5677c702a9a64b5e8c651378a8169c1958f44
Tomas Gonzalez <tfgonzalez@fi.uba.ar>
2024-06-03 22:43:50 -0300

    Add MenuItem

---
+++ src/MenuItem.ts
@@ -0,0 +1,11 @@
+class MenuItem {
+  public id: number;
+  public name: string;
+  public price: number;
+
+  public constructor(id: number, name: string, price: number) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+  }
+}
